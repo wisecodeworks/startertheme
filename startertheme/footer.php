d30ab886fde2c42a4dc9015a8364f53b27f2b485
wisecodeworks <timothyw@peta.org>
2020-11-06 17:05:58 -0500

    updated footer.php
    added the code for the footer widgets

--- startertheme/footer.php
+++ startertheme/footer.php
@@ -14,11 +14,22 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
+			<div class="col-md-4">
+			<?php dynamic_sidebar( 'footer-1' ); ?>
+		</div>
+			<div class="col-md-4">
+			<?php dynamic_sidebar( 'footer-2' ); ?>
+					</div>
+				<div class="col-md-4">
+			<?php dynamic_sidebar( 'footer-3' ); ?>
+					</div>
+<div class="col-md-12">
 			<div class="attribution">
             	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootscores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bootscores' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
 				&copy; <?php echo date('Y'); ?> <? echo get_bloginfo('name'); ?>
             </div>
+						</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
